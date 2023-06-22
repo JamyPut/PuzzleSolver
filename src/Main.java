c9b596f03f5ebfe703bc6e1fa1ac644ef46bcc70
JamyPut <jp2001dj@gmail.com>
2023-06-22 18:11:52 +0200

    first try on puzzle solver

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String puzzle = "oppfjvbikhtygrngpdmfaixoandtfzdvlnnto";
+
+        // Assuming the string is a simple substitution cipher, we can decode it by shifting each character 2 positions back in the alphabet
+        StringBuilder decodedMessage = new StringBuilder();
+        for (char c : puzzle.toCharArray()) {
+            char decodedChar = (char) (c - 2);
+            decodedMessage.append(decodedChar);
+        }
+
+        System.out.println("Decoded message: " + decodedMessage.toString());
     }
 }
