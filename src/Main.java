c5b05827cb63f07f727bd25e43d4c94e92048a79
JamyPut <jp2001dj@gmail.com>
2023-06-22 18:23:45 +0200

    rewrote solver with Caesar cipher and a for loop

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         String puzzle = "oppfjvbikhtygrngpdmfaixoandtfzdvlnnto";
 
-        // Assuming the string is a simple substitution cipher, we can decode it by shifting each character 2 positions back in the alphabet
-        StringBuilder decodedMessage = new StringBuilder();
-        for (char c : puzzle.toCharArray()) {
-            char decodedChar = (char) (c - 2);
-            decodedMessage.append(decodedChar);
-        }
+        for (int i = 0; i < 32; i++) {
+            System.out.printf("Using value %d in the Caesar cipher\n", i);
 
-        System.out.println("Decoded message: " + decodedMessage.toString());
+            StringBuilder decodedMessage = new StringBuilder();
+            for (char c : puzzle.toCharArray()) {
+                char decodedChar = (char) (c - i);
+                decodedMessage.append(decodedChar);
+            }
+
+            System.out.println("Decoded message: " + decodedMessage.toString());
+        }
     }
 }
